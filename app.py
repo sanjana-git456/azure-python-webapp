e247d0580e70bfc30a6d0b425e50131a109a732c
unknown <KJSCE@16DCEB109-06.SVV.local>
2026-02-05 12:39:47 +0530

    Testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! Python Web App Successfully Deployed on Microsoft Azure"
+    return "Hello! Python Web App Successfully Deployed on Microsoft Azure test 2"
 
 if __name__ == "__main__":
     app.run()
